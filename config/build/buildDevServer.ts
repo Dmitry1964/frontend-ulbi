7abfdc8284084f68fd5b0817ac73af470b676203
Dmytry64 <nosdm64@gmail.com>
2023-12-24 13:50:52 +0300

    Добавляет react-router, lazy-loading

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,5 +4,6 @@ export const buildDevServer = (options: BuildOptions) => {
     return {
         port: options.port,
         open: true, 
+        historyApiFallback: true,
      }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Counter } from "./componets/Counter"
+import { Routes, Route, Link } from "react-router-dom";
 import './index.scss';
+import { MainPageLazy } from "./pages/MainPage/MainPage.async";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
 
 const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to={'/'}>
+                Главная
+            </Link>
+            <Link to={'/about'}>
+                О нас
+            </Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPageLazy />} />
+                    <Route path='/about' element={<AboutPageLazy />} />
+                </Routes>
+            </Suspense>
+
         </div>
 
     )
