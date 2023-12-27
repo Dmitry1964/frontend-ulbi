fad40188e19005335bace9c2de4e1a77b8537f53
Dmytry64 <nosdm64@gmail.com>
2023-12-27 15:35:43 +0300

    Добавляет eslint

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,8 @@
-import {lazy} from "react";
+import {lazy} from 'react';
 
 export const MainPageAsync = lazy(() => new Promise(resolve => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./MainPage')), 1500)
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+  setTimeout(() => resolve(import('./MainPage')), 1500);
 }));
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 
 interface NavbarProps {
     className?: string;
@@ -8,18 +8,18 @@ interface NavbarProps {
 
 export const Navbar = ({className}: NavbarProps) => {
 
-    return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
                     Главная
-                </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to={'/about'}>
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={'/about'}>
                     О сайте
-                </AppLink>
-            </div>
-        </div>
-    );
+        </AppLink>
+      </div>
+    </div>
+  );
 };
 
 
